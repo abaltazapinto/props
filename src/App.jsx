bbd9cae7ee7d681588b6fd22c38a1c2214a0fa77
abaltaza <146941254+abaltazapinto@users.noreply.github.com>
2024-05-11 16:18:15 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+      <hr />
+      <List list={stories} />
+      <hr />
+      <Welcome text="Welcome to React" />
+      
+    </div>
+  );
+};
+
+const Welcome = ({text}) => {
+  return <h1>{text}</h1>;
+}
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
+
+const List = (props) => {
+// Destructuring to get 'list' from props
+  return (
+    <div>
+          {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+    </div>
+  )
+}
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+export default App;
